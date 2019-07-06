c15814dd911a5e0bd03647d0e0b03f67f8d193bd
abanoub essam <20130130@fa-hists.edu.eg>
2019-07-06 22:54:05 +0200

    new onea1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,8 +4,6 @@ import jwt from 'jsonwebtoken';
 
 export default {
     async Login(req, res) {
-
-
         const token = jwt.sign({id: req.user._id } , config.get('jwtPrivateKey'));
         let respo = {
             user: req.user,
